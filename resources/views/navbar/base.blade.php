c1b91621177d0394bbe9b21343d59aa1e35a398e
unknown <christiansacedon3@gmail.com>
2023-03-13 21:11:30 +0800

    first commit

---
+++ resources/views/navbar/base.blade.php
@@ -0,0 +1,45 @@
+<nav class="navbar navbar-expand-lg bg-secondary">
+    <div class="container-fluid ">
+        
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <!-- Empty placeholder to push the links to the right -->
+                <li class="nav-item"></li>
+            </ul>
+
+            <ul class="navbar-nav">
+                <li class="nav-item items-center justify-between p-4">
+                    <a class="nav-link-hover" aria-current="page" href="/">CheapTalk</a>
+                </li>
+                <li class="nav-item items-center justify-between p-4">
+                    <a class="nav-link-hover" aria-current="page" href="/posts">Posts</a>
+                </li>
+                <li class="nav-item  items-center justify-between p-4">
+                    <a class="nav-link-hover" aria-current="page" href="/categories">Categories</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<style>
+
+.navbar a {
+  padding: 0.5rem 0.75rem;
+  border-radius: 0.375rem;
+  font-size: 0.875rem;
+  font-weight: 500;
+  color: #d1d5db;
+  text-decoration: none;
+}
+
+.navbar a:hover {
+  color: #fff;
+  background-color: #4b5563;
+  background-color: rgba(75, 85, 99, 0.5);
+}
+</style>
